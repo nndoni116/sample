b31eb4b593117c4a85ceb2dce7cc02871ba98e01
nndoni116 <jljladad6342@gmail.com>
2026-02-25 19:06:46 +0900

    設定ファイルの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,11 @@
-import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare'; // 1. これを追加
 
 export default defineConfig({
   integrations: [tailwind()],
+  // 2. outputを 'server' または 'hybrid' に設定
+  output: 'server', 
+  // 3. アダプターを指定
+  adapter: cloudflare(), 
 });
