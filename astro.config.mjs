8e2fe224d9c38f0304511260c98f3091ec61e01f
QAmamomamo <hokayumi4@gmail.com>
2026-03-01 00:29:48 +0900

    astro.configを開発環境と本番環境で分岐

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare'; // 1. これを追加
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
+
+const isDev = process.env.NODE_ENV === "development";
 
 export default defineConfig({
   integrations: [tailwind()],
-  // 2. outputを 'server' または 'hybrid' に設定
-  output: 'server', 
-  // 3. アダプターを指定
-  adapter: cloudflare(), 
+
+  output: isDev ? "static" : "server",
+
+  ...(isDev
+    ? {}
+    : {
+        adapter: cloudflare(),
+        image: {
+          service: {
+            entrypoint: "@astrojs/cloudflare/image-service",
+          },
+        },
+      }),
 });
